89a09ed859556abf8c66f73c8094cad324e55578
yeongno <dldudsh83@gmail.com>
2022-07-12 16:58:50 +0900

    mianfads

--- src/components/MDTypography/index.js
+++ src/components/MDTypography/index.js
@@ -12,7 +12,7 @@ Coded by www.creative-tim.com
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-// ss
+// main
 
 import { forwardRef } from "react";
 
