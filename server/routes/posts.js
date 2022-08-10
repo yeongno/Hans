44107a1e14ab88ae70e95d9176e45eeb7e9f9a53
yeongno <dldudsh83@gmail.com>
2022-08-11 03:04:59 +0900

    completed homelanding _homelandinglist for rendering List

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -16,7 +16,7 @@ const { Post } = require("../models/Post");
 // });
 
 router.post("/getPost", (req, res) => {
-  Post.find({ public: req.body.public }).exec((err, posts) => {
+  Post.find({ topic: req.body.public }).exec((err, posts) => {
     if (err) return res.status(400).send(err);
     return res.status(200).json({ success: true, posts });
   });
